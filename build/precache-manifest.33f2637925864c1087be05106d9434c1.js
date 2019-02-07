fe7a660bae712be8e22d908cabecabc2326eb54f
Kähkönen Harri J <harrikah@lx8-523-012.pc.helsinki.fi>
2019-02-07 15:54:53 +0200

    Poistetaan gitigneeraus buildilta, jotta frontti menee minne pitää

---
+++ build/precache-manifest.33f2637925864c1087be05106d9434c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c68c7711cfd03ac49c8e",
+    "url": "/static/js/main.c68c7711.chunk.js"
+  },
+  {
+    "revision": "45a33658116e915bff91",
+    "url": "/static/js/1.45a33658.chunk.js"
+  },
+  {
+    "revision": "c68c7711cfd03ac49c8e",
+    "url": "/static/css/main.855169f5.chunk.css"
+  },
+  {
+    "revision": "a30e9d49e7a756e7affa4128280f9a56",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.b6acaf6eb7b67a579b4e3ae4cccbf150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d08172fc17386f16fe2",
+    "url": "/static/js/main.8d08172f.chunk.js"
+  },
+  {
+    "revision": "45a33658116e915bff91",
+    "url": "/static/js/1.45a33658.chunk.js"
+  },
+  {
+    "revision": "8d08172fc17386f16fe2",
+    "url": "/static/css/main.855169f5.chunk.css"
+  },
+  {
+    "revision": "00b6db6310453455a075f1f5cc7572b2",
+    "url": "/index.html"
+  }
+];
